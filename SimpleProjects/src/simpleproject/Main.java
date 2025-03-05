68a05dee99cd00a4237a90ec19ce53754e11bece
BRANDON LAFFERTY <1636821@fife.ac.uk>
2025-03-05 11:57:11 +0000

    Added basic project files

---
+++ SimpleProjects/src/simpleproject/Main.java
@@ -0,0 +1,11 @@
+package simpleproject;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SimpleProjectCode s = new SimpleProjectCode();
+		s.run();
+	}
+
+}
---
+++ SimpleProjects/src/simpleproject/SimpleProjectCode.java
@@ -0,0 +1,10 @@
+package simpleproject;
+
+public class SimpleProjectCode {
+
+	public void run() {
+		System.out.println("Hello");
+		
+	}
+	
+}
